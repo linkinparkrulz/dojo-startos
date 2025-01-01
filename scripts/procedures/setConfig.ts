6503870da7778dd16517dc4a407497f993ca38ad
Eric <eric@localhost>
2025-01-01 17:32:23 -0600

    Add Bitcoin testnet option

--- scripts/procedures/setConfig.ts
+++ scripts/procedures/setConfig.ts
@@ -1,8 +1,24 @@
 import { compat, types as T } from "../deps.ts";
 
-export const setConfig: T.ExpectedExports.setConfig = async (effects, input ) => {
+// deno-lint-ignore require-await
+export const setConfig: T.ExpectedExports.setConfig = async (
+  effects: T.Effects,
+  newConfig: T.Config,
+) => {
+  // deno-lint-ignore no-explicit-any
+  const dependsOnBitcoind: { [key: string]: string[] } =
+    (newConfig as any) ?.['bitcoin-node']?.type === 'bitcoind'
+      ? { 'bitcoind': ['synced'] }
+      : {};
 
-  const depsBitcoind: T.DependsOn = {bitcoind: ['synced']}
+  // deno-lint-ignore no-explicit-any
+  const dependsOnBitcoindTestnet: { [key: string]: string[] } =
+    (newConfig as any) ?.['bitcoin-node']?.type === 'bitcoind-testnet'
+      ? { 'bitcoind-testnet': ['synced'] }
+      : {};
 
-  return await compat.setConfig(effects,input, depsBitcoind)
-}
+  return compat.setConfig(effects, newConfig, {
+    ...dependsOnBitcoind,
+    ...dependsOnBitcoindTestnet,
+  });
+};
