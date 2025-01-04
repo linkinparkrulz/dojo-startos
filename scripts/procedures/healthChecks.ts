b6d7d548ede90aafdf698e6e57d81dfbb82b2241
Eric P <ericpp@gmail.com>
2025-01-03 21:51:09 -0600

    Move web check to /static/admin/

--- scripts/procedures/healthChecks.ts
+++ scripts/procedures/healthChecks.ts
@@ -2,7 +2,7 @@ import { types as T, healthUtil } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
   async "web-ui"(effects, duration) {
-    return healthUtil.checkWebUrl("http://dojo.embassy:9000")(effects, duration)
+    return healthUtil.checkWebUrl("http://dojo.embassy:9000/static/admin/")(effects, duration)
       .catch(healthUtil.catchError(effects))
   },
   async "api"(effects, duration) {
