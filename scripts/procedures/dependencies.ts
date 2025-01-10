28614bed998539f246b8d4f3c7ec736e7789b3a8
Eric P <ericpp@gmail.com>
2025-01-10 01:08:06 -0600

    Require bitcoind/bitcoind-testnet to run in full mode

--- scripts/procedures/dependencies.ts
+++ scripts/procedures/dependencies.ts
@@ -32,12 +32,15 @@ export const dependencies: T.ExpectedExports.dependencies = {
     // deno-lint-ignore require-await
     async check(effects, config) {
       effects.info("check bitcoind");
+
       if (!matchBitcoindConfig.test(config)) {
         return { error: "Bitcoind config is not the correct shape" }
       }
+
       if (!config.rpc.enable) {
         return { error: "Must have RPC enabled" };
       }
+
       // if (config.advanced.pruning.mode !== "disabled") {
       //   return { error: "Pruning must be disabled (must be an archival node)" };
       // }
@@ -46,24 +49,83 @@ export const dependencies: T.ExpectedExports.dependencies = {
       //     error:
       //       "Must have block filter index enabled for Run The Numbers to work",
       //   };
+
       // }
       // if (config.rpc.advanced.threads < 4) {
       //   return { error: "Must be greater than or equal to 4" };
       // }
+
       return { result: null };
     },
     // deno-lint-ignore require-await
     async autoConfigure(effects, configInput) {
       effects.info("autoconfigure bitcoind");
+
       const config = matchBitcoindConfig.unsafeCast(configInput);
+
       config.rpc.enable = true;
-      // config.advanced.blockfilters.blockfilterindex = true;
-      // if (config.rpc.advanced.threads < 4) {
-      //   config.rpc.advanced.threads = 4;
-      // }
+
       // if (config.advanced.pruning.mode !== "disabled") {
       //   config.advanced.pruning.mode = "disabled";
       // }
+
+      // config.advanced.blockfilters.blockfilterindex = true;
+
+      // if (config.rpc.advanced.threads < 4) {
+      //   config.rpc.advanced.threads = 4;
+      // }
+
+      return { result: config };
+    },
+  },
+  'bitcoind-testnet': {
+    // deno-lint-ignore require-await
+    async check(effects, config) {
+      effects.info("check bitcoind-testnet");
+
+      if (!matchBitcoindConfig.test(config)) {
+        return { error: "Bitcoind-testnet config is not the correct shape" }
+      }
+
+      if (!config.rpc.enable) {
+        return { error: "Must have RPC enabled" };
+      }
+
+      if (config.advanced.pruning.mode !== "disabled") {
+        return { error: "Pruning must be disabled (must be an archival node)" };
+      }
+
+      if (!config.advanced.blockfilters.blockfilterindex) {
+        return {
+          error:
+            "Must have block filter index enabled for Run The Numbers to work",
+        };
+      }
+
+      // if (config.rpc.advanced.threads < 4) {
+      //   return { error: "Must be greater than or equal to 4" };
+      // }
+
+      return { result: null };
+    },
+    // deno-lint-ignore require-await
+    async autoConfigure(effects, configInput) {
+      effects.info("autoconfigure bitcoind");
+
+      const config = matchBitcoindConfig.unsafeCast(configInput);
+
+      config.rpc.enable = true;
+
+      if (config.advanced.pruning.mode !== "disabled") {
+        config.advanced.pruning.mode = "disabled";
+      }
+
+      config.advanced.blockfilters.blockfilterindex = true;
+
+      // if (config.rpc.advanced.threads < 4) {
+      //   config.rpc.advanced.threads = 4;
+      // }
+
       return { result: config };
     },
   },
