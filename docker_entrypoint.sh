0b7333d56d782ac44e7a15278844d01765a1eb75
Eric P <ericpp@gmail.com>
2025-01-11 13:07:58 -0600

    Fix MySQL table creation

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -73,14 +73,9 @@ EOF
 
 	rm -f $tfile
 
-	for f in /docker-entrypoint-initdb.d/*; do
-		case "$f" in
-			*.sql)    echo "$0: running $f"; cat "$f" | sed "1iUSE \`$MYSQL_DATABASE\`;" | /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0; echo ;;
-			*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | sed "1iUSE \`$MYSQL_DATABASE\`;" | /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-			*)        echo "$0: ignoring or entrypoint initdb empty $f" ;;
-		esac
-		echo
-	done
+	# install database
+	echo 'Creating tables'
+	sed "1iUSE \`$MYSQL_DATABASE\`;" /docker-entrypoint-initdb.d/1_db.sql.tpl | /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0
 
 	echo
 	echo 'MySQL init process done. Starting mysqld...'
