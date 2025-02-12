b9c1f10abea86dd1a2ae12397d0dbb2eef583b6d
Eric P <ericpp@gmail.com>
2025-02-11 22:12:34 -0600

    Remove healthChecks.ts which was replaced by bash scripts

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -2,5 +2,4 @@ export { setConfig } from "./procedures/setConfig.ts";
 export { getConfig } from "./procedures/getConfig.ts";
 export { properties } from "./procedures/properties.ts";
 export { migration } from "./procedures/migrations.ts";
-export { health } from "./procedures/healthChecks.ts";
 export { dependencies } from "./procedures/dependencies.ts";
